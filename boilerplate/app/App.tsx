f612dd8e452ed6ea2419fd0aca0c826263d2bb9e
polnikale <nikpolale@gmail.com>
2021-07-10 15:07:25 +0300

    feat: add layout animations on android

--- boilerplate/app/App.tsx
+++ boilerplate/app/App.tsx
@@ -1,7 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import { Platform, UIManager } from 'react-native';
 import 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+if (Platform.OS === 'android') {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
 
 const App = () => {
   return (
